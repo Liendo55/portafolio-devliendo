a5c431136b6e429ad29ddb989c040f8e32e738e0
Liendo55 <jesusliendo55@gmail.com>
2025-02-25 13:09:03 -0600

    fix(modules): remove modules of index

--- src/app/layout/nav-bar/nav-bar.component.ts
+++ src/app/layout/nav-bar/nav-bar.component.ts
@@ -2,18 +2,10 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { SkillComponent } from '../../pages/skill/skill.component';
 import { AboutMeComponent } from '../../pages/about-me/about-me.component';
-import { ContactComponent } from '../../pages/contact/contact.component';
-import { TestimonyComponent } from '../../pages/testimony/testimony.component';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [
-    FooterComponent,
-    SkillComponent,
-    AboutMeComponent,
-    ContactComponent,
-    TestimonyComponent,
-  ],
+  imports: [FooterComponent, SkillComponent, AboutMeComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
