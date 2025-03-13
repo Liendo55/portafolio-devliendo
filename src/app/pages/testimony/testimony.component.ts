55be8255fe6e0820ffd028e08ea10a0557727c39
Liendo55 <jesusliendo55@gmail.com>
2025-03-13 13:04:51 -0600

    fix: updated module in use testimony real

--- src/app/pages/testimony/testimony.component.ts
+++ src/app/pages/testimony/testimony.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-testimony',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './testimony.component.html',
-  styleUrl: './testimony.component.css'
+  styleUrl: './testimony.component.css',
 })
-export class TestimonyComponent {
-
-}
+export class TestimonyComponent {}
